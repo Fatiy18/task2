88d438a0096746d94265f9a583cd05ad9b84f88f
Fatiy18 <152783789+Fatiy18@users.noreply.github.com>
2023-12-20 12:51:03 +0400

    task2

---
+++ index.js
@@ -0,0 +1,62 @@
+// task1
+
+// function bazar(){
+//     let date = new Date(year ,0 ,1)
+// }
+
+
+
+// task4
+
+
+// function hesabla(qiymet,imtahan){
+//     if(imtahan==="yekun imtahan" && qiymet>=90){
+//         alert("A+");
+//     }else if(qiymet>=89 && qiymet<=100){
+//         alert("A+");
+//     }else{
+//         alert("kesildin ayy yazigg");
+//     }
+// }
+
+// let qiymet=prompt("qiymetinizi daxil edin");
+// let imtahan=prompt("Imtahan novunuzu daxil edin");
+
+
+// hesabla(qiymet,imtahan);
+
+// task3
+
+// let a=prompt("a ededini daxil edin");
+// let b=prompt("b ededini daxil edin");
+
+// let vur=parseInt(a)*parseInt(b);
+// console.log(vur);
+
+// let bol=parseInt(a)/parseInt(b);
+// if(b==0){
+//     console.log("0 a bolme yoxdur");
+// }else{
+//     console.log(bol);
+// }
+
+
+// task1
+
+
+// function date(year){
+//     let yanvar1= new Date(year,0,1);
+    
+
+
+//     switch(hefteGun){
+//         case 0:
+//             console.log("true");
+//         case
+//     }
+// }
+
+
+
+
+
